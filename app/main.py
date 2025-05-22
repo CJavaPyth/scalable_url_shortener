b3ff6556912efb7ac37f92d3d7d20fb436c8b657
CJavaPyth <glenntay131@gmail.com>
2025-05-22 17:43:51 +0800

    Added port mapping for app container, added models.py

--- app/main.py
+++ app/main.py
@@ -2,16 +2,22 @@ from fastapi import FastAPI, Depends, Request, HTTPException
 from sqlalchemy.orm import Session
 
 import crud
+from models import UrlRequest
 from database import get_db, init_db
 import string
 import random
 from cache import is_rate_limited
-from fastapi.staticfiles import StaticFiles
-
+from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
+from pathlib import Path
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def serve_root():
+    html_path = Path("static/index.html")
+    return html_path.read_text()
 
 
 @app.on_event("startup")
@@ -24,7 +30,7 @@ def generate_code(length=6):
 
 
 @app.post("/shorten")
-def shorten(request: Request, url: str, db: Session = Depends(get_db)):
+def shorten(request: Request, data: UrlRequest, db: Session = Depends(get_db)):
     client_ip = request.client.host
     if is_rate_limited(client_ip):
         raise HTTPException(status_code=429, detail="Too many requests")
@@ -33,7 +39,7 @@ def shorten(request: Request, url: str, db: Session = Depends(get_db)):
     while crud.short_code_exists(db, code):
         code = generate_code()
 
-    return crud.create_short_url(db, url, code)
+    return crud.create_short_url(db, data.url, code)
 
 
 @app.get("/{short_code}")
@@ -41,4 +47,4 @@ def redirect(short_code: str, db: Session = Depends(get_db)):
     original = crud.get_original_url(db, short_code)
     if not original:
         raise HTTPException(status_code=404, detail="Short URL not found")
-    return {"original_url": original}
+    return RedirectResponse(original)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
 
 
-class ShortenRequest(BaseModel):
+class UrlRequest(BaseModel):
     url: str
